2285176b8d5b38937a84ea96921c14f502b6feaa
Crouzzz <Yousef.AlJorah.eam.js@tuwaiq.edu.sa>
2021-12-05 00:58:54 +0300

    quiz2

--- quiz 2/bachend/server.js
+++ quiz 2/bachend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const {data} = require('./db')
-const {userRouter}=require('./userRouter')
+// const {userRouter}=require('./userRouter')
 const app = express();
 
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //third-party middleware
 app.use(cors());
-app.use('/data',userRouter)
+// app.use('/data',userRouter)
 
 
 const PORT = process.env.PORT || 3050;
@@ -18,6 +18,30 @@ const PORT = process.env.PORT || 3050;
 app.listen(PORT, () => {
 	console.log(`Server On ${PORT}`);
 });
+// task 1
 app.get('/', (req, res) => {
 	res.send(data)
 });
+// task 2
+// app.get('gitbyid/:id',(req,res)=>{
+// 	const yousef = data.filter(
+// 		(elem)=>elem.id===(req.params.id)
+// 	)
+// 	res.send(yousef);
+// })
+
+app.get('/getById/:id', (req, res) => {
+	const youseff = data.filter(
+	  (elem) => elem.id === parseInt(req.params.id)
+	);
+	res.send(youseff);
+  });
+  //task 3
+  app.get('/getRandom', (req, res) => {
+	const youseff = Math.floor(Math.random()*5)
+	const random=data.filter(
+		(elem)=> elem.id===youseff
+	)
+	res.send(random);
+  });
+  
