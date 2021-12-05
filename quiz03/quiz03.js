45ab1d9f0fdb7681eb49b85a9377fad4968e1f8d
Crouzzz <Yousef.AlJorah.eam.js@tuwaiq.edu.sa>
2021-12-05 10:08:18 +0300

    quiz3

---
+++ quiz03/quiz03.js
@@ -0,0 +1,45 @@
+
+
+
+//q 2
+// const sumOddNumber = function(arr){
+//     /*         write your code here        */
+//     let oddNum=0;
+    
+//     for(i=0;i<arr.length;i++){
+//         if(arr[i]%2==1){
+//             oddNum+=arr[i]
+//         }
+//     }
+//     return oddNum
+//     }
+//     let arr=[1,2,3,4,5]
+//     console.log(sumOddNumber(arr)); 
+
+//     // q 6
+//     const containsElements = function(arr1, arr2){
+//         /*         write your code here        */
+//         for(i=0;i<)
+//         }
+
+        //
+        // const flattenArray = function(arr){
+            /*         write your code here        */
+            // let arrr=[];
+            // let x=[]
+            // x= arrr.concat(arr)
+            // return x
+            // }
+            // let arr=[0,1,2,[3,4]]
+            // console.log(flattenArray(arr));
+            /// powe of 2 or not 
+
+            const isPowerOfTwo = function(num){
+                /*         write your code here        */
+                if(num%2==0){
+                    return console.log("yes");
+                } else 
+                return console.log('no');
+                
+                }
+                isPowerOfTwo(4)
